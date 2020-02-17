575557d4398174371c090b92a89235c5a826f982
Jin <jintang413@gmail.com>
2020-02-17 16:32:06 -0500

    completed quick find implementation

--- algo/unionfind.py
+++ algo/unionfind.py
@@ -1,20 +1,29 @@
+from utils.util import is_within_range
+
+
 class QuickFindUF:
 
     def __init__(self, n: int):
-        self._id = range(0, n)
-        self._count = n
+        self._n = n
+        self._id = list(range(n))
+        self._validate_range = lambda x: is_within_range(x, ub=n-1)
 
     def union(self, p: int, q: int):
-        raise NotImplemented
+        pid = self.find(p)
+        qid = self.find(q)
+        for i in range(self._n):
+            self._id[i] = qid if self._id[i] == pid else self._id[i]
 
     def connected(self, p: int, q: int):
-        raise NotImplemented
+        return self.find(p) == self.find(q)
 
     def find(self, p: int) -> int:
-        raise NotImplemented
+        if not self._validate_range(p):
+            raise ValueError(f'Input of value {p} is not between 0 and {self._n - 1}.')
+        return self._id[p]
 
     def count(self) -> int:
-        return self._count
+        return len(set(self._id))
 
 
 class QuickUnionUF:
---
+++ utils/util.py
@@ -0,0 +1,2 @@
+def is_within_range(v, ub: int, lb : int=0) -> bool:
+    return v >= lb and v <= ub
