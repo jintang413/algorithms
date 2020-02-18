28122fb9ce207d9a9df22c21da784623524ba077
Jin <jintang413@gmail.com>
2020-02-17 21:08:49 -0500

    weighted with compression

--- algo/unionfind.py
+++ algo/unionfind.py
@@ -114,21 +114,39 @@ class QuickUnionPathCompressionUF:
 
 class WeightedQuickUnionPathCompressionUF:
     def __init__(self, n: int):
-        self._id = range(0, n)
+        self._n = n
+        self._id = list(range(n))
         self._sz = [1] * n
-        self._count = n
+        self._validate_range = lambda x: is_within_range(x, ub=n-1)
 
-    def _root(self, i: int) -> int:
-        raise NotImplemented
+    def _root(self, p: int) -> int:
+        return recurse_by_index(self._id, p, compress=True)
 
     def union(self, p: int, q: int):
-        raise NotImplemented
+        root_p = self.find(p)
+        root_q = self.find(q)
+        if root_p == root_q:
+            return
+
+        if self._sz[root_p] < self._sz[root_q]:
+            self._id[root_p] = root_q
+            self._sz[root_q] = 1
+            self._sz[root_p] = 1
+            self._sz[root_q] += self._sz[root_p]
+        else:
+            self._id[root_q] = root_p
+            self._sz[root_q] = 1
+            self._sz[root_p] = 1
+            self._sz[root_p] += self._sz[root_q]
 
     def connected(self, p: int, q: int):
-        raise NotImplemented
+        return self.find(p) == self.find(q)
 
     def find(self, p: int) -> int:
-        raise NotImplemented
+        if not self._validate_range(p):
+            raise ValueError(f'Input of value {p} is not between 0 and {self._n - 1}.')
+        return self._root(p)
 
     def count(self) -> int:
-        return self._count
+        roots = set([self._root(i) for i in self._id])
+        return len(roots)
--- utils/util.py
+++ utils/util.py
@@ -1,9 +1,11 @@
-def is_within_range(v, ub: int, lb : int=0) -> bool:
+def is_within_range(v, ub: int, lb: int = 0) -> bool:
     return v >= lb and v <= ub
 
 
-def recurse_by_index(arr, i):
+def recurse_by_index(arr, i, compress=False):
     if i == arr[i]:
         return i
     else:
+        if compress:
+            arr[i] = arr[arr[i]]
         return recurse_by_index(arr, arr[i])
