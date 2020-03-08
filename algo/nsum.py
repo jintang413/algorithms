5d681c364a902a06e963b24ad5699f162bd219f9
jintang413 <30931294+jintang413@users.noreply.github.com>
2020-03-08 10:10:14 -0400

    updated nsum brute force

--- algo/nsum.py
+++ algo/nsum.py
@@ -4,12 +4,10 @@ class NSum:
     def two_sum_brute_force(arr, sum_to):
         n = len(arr) - 1
         for i in range(0, n - 1):
-            for j in range(1, n):
+            for j in range(i + 1, n):
                 if arr[i] + arr[j] == sum_to:
                     print(arr[i], arr[j])
-                    return True
 
-        return False
     @staticmethod
     def two_sum_sort(arr, sum_to):
         raise NotImplemented
@@ -22,14 +20,12 @@ class NSum:
     def three_sum_brute_force(arr, sum_to):
         n = len(arr) - 1
         for i in range(0, n - 2):
-            for j in range(1, n - 1):
-                for k in range(2, n):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
                     if arr[i] + arr[j] + arr[k] == sum_to:
-                        print(arr[i],  arr[j], arr[k])
-                        return True
+                        print(arr[i], arr[j], arr[k])
 
         return False
-
 
     @staticmethod
     def three_sum_sort(arr, sum_to):
